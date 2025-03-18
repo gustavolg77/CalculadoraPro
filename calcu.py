1d3c0058f8e45ae1dba8648e372d12d99a5b30bf
Luis Gustavo <luisgustavogarcia576@gmail.com>
2025-03-18 19:16:28 -0400

    Primer commit: Se agregan archivos.md y el archivo calcu.py

---
+++ calcu.py
@@ -0,0 +1,39 @@
+#menu principal de la calculadora
+
+
+
+#funcion suma
+
+
+
+#funcion resta
+
+
+#funcion multiplicacion
+
+
+
+#funcion division
+
+
+
+
+#funcion de potencia
+
+
+
+
+#funcion de raiz cuadrada
+
+
+
+
+#funcion de modulo (resto de la division)
+
+
+
+#funcion de factorial
+
+
+
+#funcion de logaritmo
